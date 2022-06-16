395934dae950656ce3353a517fe17d632c8ec5e5
Alex Lopez <alex.lopez@rokubun.cat>
2022-06-16 15:59:26 +0200

    chore: do not write logs to stdout with quiet options

--- generate-tiles.sh
+++ generate-tiles.sh
@@ -37,14 +37,14 @@ fi
 
 # generate mbtiles (lvl zoom [1-9], base zoom 1, no tile compressed)
 if [ -f "$STATIONS_GEOJSON" ]; then
-    tippecanoe --no-tile-compression -z9 -Z1 -f -B1 -o $STATIONS_MBTILES $STATIONS_GEOJSON
+    tippecanoe --no-tile-compression -z9 -Z1 -f -B1 --quiet -o $STATIONS_MBTILES $STATIONS_GEOJSON
 else
     echo "file $STATIONS_GEOJSON does not exist." 
     exit 1
 fi
 #Export an mbtiles file to a directory of files
 if [ -f "$STATIONS_MBTILES" ]; then    
-    mb-util --image_format=pbf   $STATIONS_MBTILES $TILES_FOLDER
+    mb-util --image_format=pbf --silent $STATIONS_MBTILES $TILES_FOLDER
     echo "YOU SHOULD UPLOAD TILES FOLDER TO S3 BUCKET (jason-coverage-tiles)"      
 else
     echo "file $STATIONS_MBTILES does not exist."
