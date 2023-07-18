f3841b9eb27e9b4216f93546143cf9b24ee29c4a
Alex Lopez <alex.lopez@rokubun.cat>
2023-07-18 12:42:34 +0200

    fix: set name of the layer to stations

--- generate-tiles.sh
+++ generate-tiles.sh
@@ -37,11 +37,13 @@ fi
 
 # generate mbtiles (lvl zoom [1-9], base zoom 1, no tile compressed)
 if [ -f "$STATIONS_GEOJSON" ]; then
-    tippecanoe --no-tile-compression -z9 -Z1 -f -B1 --quiet -o $STATIONS_MBTILES $STATIONS_GEOJSON
+
+    tippecanoe --no-tile-compression -z9 -Z1 -f -B1 --quiet -l stations -o $STATIONS_MBTILES $STATIONS_GEOJSON
 else
     echo "file $STATIONS_GEOJSON does not exist." 
     exit 1
 fi
+
 #Export an mbtiles file to a directory of files
 if [ -f "$STATIONS_MBTILES" ]; then    
     mb-util --image_format=pbf --silent $STATIONS_MBTILES $TILES_FOLDER
